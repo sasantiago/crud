3cac11d2239a1acc6fb6bfa297383f45f52b6312
JoanZapata05 <zapatajoan05@gmail.com>
2024-05-08 09:07:35 -0500

    feat: creacion de tabla con listas

---
+++ src/js/database.js
@@ -0,0 +1,140 @@
+export let coders = [
+    {
+        id: 1,
+        name: "javier",
+        lastName: "combita",
+        email: "jcomte23@outlook.com"
+    },
+    {
+        id: 2,
+        name: 'jhonatan',
+        lastName: 'toro',
+        email: 'jtorova5@gmail.com',
+    },
+    {
+        id: 3,
+        name: "Juan",
+        lastName: "Ruiz",
+        email: "Juan@example.com"
+    },
+    {
+        id: 4,
+        name: "samuel",
+        lastName: "villarreal duque",
+        email: "samuvv2@gmail.com"
+    },
+    {
+        id: 5,
+        name: "luis Alejandro",
+        lastName: "londoño Valle",
+        email: "alejandrolondoñovalle@gmail.com"
+    },
+    {
+        id: 6,
+        name: "mariana",
+        lastName: "martinez",
+        email: "marimarhola16@gmail.com"
+    },
+    {
+        id: 7,
+        name: "Jonathan",
+        lastName: "escobar",
+        email: "EscobarjonathanM@outlook.es"
+    },
+    {
+        id: 8,
+        name: "Luisa",
+        lastName: "Porras",
+        email: "luisa@gmail.com"
+    },
+    {
+        id: 9,
+        name: "Daniel",
+        lastName: "Cano",
+        email: "dacardonac201@gmail.com"
+    },
+    {
+        id: 10,
+        name: "lisseth",
+        lastName: "benjumea",
+        email: "lbenjumb07@gmail.com"
+    },
+    {
+        id: 11,
+        name: "joancito",
+        lastName: "zapata",
+        email: "zapatajoan05@gmail.com"
+    },
+    {
+        id: 12,
+        name: "sebas",
+        lastName: "ortiz",
+        email: "sebas@gmail.com"
+    },
+    {
+        id: 13,
+        name: "arlex",
+        lastName: "zapata",
+        email: "arlex@gmail.com"
+    },
+    {
+        id: 14,
+        name: "Luisa",
+        lastName: "Cardona",
+        email: "luisaCardona@gmail.com"
+    },
+    {
+        id: 15,
+        name: "Mateo",
+        lastName: "Montoya Ospina",
+        email: "mateomoospina@gmail.com",
+    },
+    {
+        id: 16,
+        name: "Simòn",
+        lastName: "Arias",
+        email: "sariass077@gmail.com",
+    },
+    {
+        id: 17,
+        name: "John",
+        lastName: "garces",
+        email: "juanpagacevedo@gmail.com",
+    },
+    {
+        id: 18,
+        name: "Eloy",
+        lastName: "Sanchez",
+        email: "johaneloysanchezhernandez@gmail.com"
+    },
+    {
+        id: 19,
+        name: "david",
+        lastName: "blandon",
+        email: "franccoina@gmail.com"
+    },
+    {
+        id: 20,
+        name: "Herbin",
+        lastName: "Esteban",
+        email: "Herbingg@gmail.com",
+    },
+    {
+        id: 21,
+        name: "Nick",
+        lastName: "Martinez",
+        email: "nicolarmarcar2006@gmail.com"
+    },
+    {
+        id: 22,
+        name: "Juan",
+        lastName: "Quirama",
+        email: "jquirama@gmail.com"
+    },
+    {
+        id: 23,
+        name: "alejandro",
+        lastName: "gomez",
+        email: "alejogomez@gmail.com"
+    }
+]
--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,44 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+import {coders} from './database.js'
+
+const tbody = document.querySelector("tbody")
+
+
+// function listawithinner(){
+//     coders.forEach(coder =>{
+//         tbody.innerHTML +=`
+//         <tr>
+//         <th scope="row">${coder.id}</th>
+//         <td>${coder.name}</td>
+//         <td>${coder.lastName}</td>
+//         <td>${coder.email}
+//     `
+    
+//     })
+    
+// }
+
+function listawithBasicElemnet() {
+    coders.forEach(coder => {
+        const tr = document.createElement("tr")
+
+        for (let i = 0; i < 4; i++) {
+            const td = document.createElement("td")
+            tr.appendChild(td)
+            
+        }
+
+        // tr.firstElementChild.textContent=coder.id
+        // tr.firstElementChild.nextElementSibling.textContent=coder.name
+        // tr.firstElementChild.nextElementSibling.nextElementSibling.textContent=coder.lastName
+        // tr.lastElementChild.textContent=coder.email
+
+        tr.children[i].textContent = coder.id
+
+        tbody.appendChild(tr)
+    });
+}
+listawithBasicElemnet()
